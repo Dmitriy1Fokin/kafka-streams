d875743eb2903e65b4104cee4b56af1c494cca4b
Dmitriy Fokin <dmitrii.fokin@aisa.ru>
2020-10-19 14:38:02 +0300

    first commit

---
+++ src/test/java/ru/fds/kafka/stream/KafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fds.kafka.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
